e6eb6aad0c9af850c4b1fc7fa84aae12fdaac091
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2022-06-17 20:29:17 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 
 const connect = require("./config/db.js");
 const productcontroller = require("./controller/productcontroller");
